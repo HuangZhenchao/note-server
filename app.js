71d0a71e69148075ce4d0cf08bebe1062be973ef
HuangZhenchao <1746565921@qq.com>
2022-03-18 18:21:22 +0800

    0.50.1 build

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const history = require('connect-history-api-fallback')
 app.use('/', history());
 app.use('/upload',express.static(path.join(__dirname, './upload')))
 app.use('/public',express.static(path.join(__dirname, './public/')))
-app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, './dist')))
 // 解析 application/json
 app.use(bodyParser.json()); 
 // 解析 application/x-www-form-urlencoded
